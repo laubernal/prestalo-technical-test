0ae41a8f2bdcb10d66ecae2d5387b7434e9dce8a
laubernal <lbernalrodrguez@gmail.com>
2025-03-23 10:51:11 +0100

    feat: implement api methods

---
+++ client/src/api/api.ts
@@ -0,0 +1,36 @@
+import {LoanApplication} from "@/types/loanApplication";
+import {Offer} from "@/types/offer";
+import {LoanApplicationResponse} from "@/types/loanApplicationResponse";
+import {ApiResponse} from "@/types/apiResponse";
+
+const API_URL = "http://localhost:3000/api";
+
+export const fetchLoanApplications = async (): Promise<LoanApplicationResponse> => {
+    try {
+        const response: Response = await fetch(`${API_URL}/loans`, {method: 'GET'})
+
+        const apiResponse: ApiResponse = await response.json();
+
+        return apiResponse.data;
+    } catch (error: any) {
+        throw new Error(error.response?.data?.message || "Error fetching requests");
+    }
+}
+
+export const createLoanApplication = async (loanApplication: LoanApplication): Promise<Offer[]> => {
+    try {
+        const response: Response = await fetch(`${API_URL}/loans`, {
+            method: 'POST',
+            body: JSON.stringify(loanApplication),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+
+        const apiResponse: ApiResponse = await response.json();
+
+        return apiResponse.data;
+    } catch (error: any) {
+        throw new Error(error.response?.data?.message || "Error fetching requests");
+    }
+}
