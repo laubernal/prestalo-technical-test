8b36cb3223dc883bbfc506ebf40bf985b6afc145
laubernal <lbernalrodrguez@gmail.com>
2025-03-23 10:51:34 +0100

    feat: enable cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,11 @@ import {AppModule} from "@shared/Nest/AppModule";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:3001',
+    ],
+  });
   await app.listen(3000);
 }
 bootstrap();
